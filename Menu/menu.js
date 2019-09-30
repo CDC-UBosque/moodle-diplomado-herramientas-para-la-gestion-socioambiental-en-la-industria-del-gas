bc616862bd2ccb0b4c67173d77389d6905d9a6b0
Unknown <seguragabriel@unbosque.edu.co>
2019-09-30 17:06:34 -0500

    wip sep 30

---
+++ Menu/menu.js
@@ -0,0 +1,55 @@
+// banner mod 0
+$('#banner-0').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/leucemia-amgen/images/banners/home.jpg" alt="Diplomado imagen"/>');
+
+// banner mod 1
+$('#banner-1').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/leucemia-amgen/images/banners/home.jpg" alt="Diplomado imagen"/>');
+
+// banner mod 2
+$('#banner-2').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/leucemia-amgen/images/banners/home.jpg" alt="Diplomado imagen"/>');
+
+// banner mod 3
+$('#banner-3').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/leucemia-amgen/images/banners/home.jpg" alt="Diplomado imagen"/>');
+
+// Menu copy
+//$('#menu').html('<div id="menuanchor-top"><h5>Menú</h5><nav id="side-navbar"><a class="side-navbar-a" href="#contact">Inicio</a><button class="drop-button">Módulo 0</button><div id="dropdown-container"><a href="#">Link 1</a><a href="#">Link 2</a><a href="#">Link 3</a></div><button class="drop-button">Módulo 1</button><div id="dropdown-container"><a href="#">Link</a></div><button class="drop-button">Módulo 2</button><div id="dropdown-container"><a href="#">Link</a></div><button class="drop-button">Módulo 3</button><div id="dropdown-container"><a href="#">Link</a></div><button class="drop-button">Módulo 4</button><div id="dropdown-container"><a href="#">Link</a></div><button class="drop-button">Módulo 5</button><div id="dropdown-container"><a href="#">Link</a></div><button class="drop-button eva">Evaluaciones</button><div id="dropdown-container"><a href="#">Evaluación Módulo 1</a><a href="#">Evaluación Módulo 2</a><a href="#">Evaluación Módulo 3</a><a href="#">Evaluación Módulo 4</a><a href="#">Evaluación Módulo 5</a></div></nav></div>');
+
+
+// footer curso amgen #1
+$('#footer').html('<div><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/leucemia-amgen/images/svg/logo-AMGEN.svg" alt="AMGEN"/></div><div><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/leucemia-amgen/images/svg/logo-UEB.svg" alt="Universidad El Bosque"/></div><div><span>© Todos los derechos reservados</span></div>');
+
+
+//
+// barras progreso
+//
+// progreso 10%
+$('#progreso10').html('<div class="progress progress-striped active"><div class="bar" style="width: 10%;"></div></div>');
+// progreso 20%
+$('#progreso20').html('<div class="progress progress-striped active"><div class="bar" style="width: 20%;"></div></div>');
+// progreso 30%
+$('#progreso30').html('<div class="progress progress-striped active"><div class="bar" style="width: 30%;"></div></div>');
+// progreso 40%
+$('#progreso40').html('<div class="progress progress-striped active"><div class="bar" style="width: 40%;"></div></div>');
+// progreso 50%
+$('#progreso50').html('<div class="progress progress-striped active"><div class="bar" style="width: 50%;"></div></div>');
+// progreso 60%
+$('#progreso60').html('<div class="progress progress-striped active"><div class="bar" style="width: 60%;"></div></div>');
+// progreso 70%
+$('#progreso70').html('<div class="progress progress-striped active"><div class="bar" style="width: 70%;"></div></div>');
+// progreso 80%
+$('#progreso80').html('<div class="progress progress-striped active"><div class="bar" style="width: 80%;"></div></div>');
+// progreso 90%
+$('#progreso90').html('<div class="progress progress-striped active"><div class="bar" style="width: 90%;"></div></div>');
+// progreso 100%
+$('#progreso100').html('<div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div>');
+
+
+// material mod 1
+$('#material-mod-1').html('<div class="wow fadeInUp"> <div class="disclaimers"> <span>SC-COL-NP-00211<br/>Patrocinado por: AMGEN Biotecnológica S.A.S.<br/>Material dirigido exclusivamente al profesional de la salud<br/>Para mayor información favor contacte a <a href="mailto:medicoscolombia@amgen.com">medicoscolombia@amgen.com</a></span> </div></div>');
+
+$('#material-mod-2').html('<div class="wow fadeInUp"> <div class="disclaimers"> <span>SC-COL-NP-00212<br/>Patrocinado por: AMGEN Biotecnológica S.A.S.<br/>Material dirigido exclusivamente al profesional de la salud<br/>Para mayor información favor contacte a <a href="mailto:medicoscolombia@amgen.com">medicoscolombia@amgen.com</a></span> </div></div>');
+
+$('#material-mod-3').html('<div class="wow fadeInUp"> <div class="title"> <h2>Material del módulo en progreso</h2> </div><div class="thumbnails"> <div class="thumbnail"><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/leucemia-amgen/images/mod3/pdf01.jpg"> <div class="caption"> <h3>Presentación PDF </h3> <p>Advances in adult acute lymphoblastic leukemia therapy</p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=308544&chapterid=9156" class="btn btn-primary">Ver Presentación </a> </p></div></div><div class="thumbnail"><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/leucemia-amgen/images/mod3/pdf02.jpg"> <div class="caption"> <h3>Presentación PDF </h3> <p>Clinical practice guidelines in oncology / Acute lymphoblastic leukemia</p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=308544&chapterid=9157" class="btn btn-primary">Ver Presentación </a> </p></div></div><div class="thumbnail"><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/leucemia-amgen/images/mod3/pdf03.jpg"> <div class="caption"> <h3>Presentación PDF </h3> <p>Guía de práctica clínica para la detección, tratamiento y seguimiento de leucemia linfoblástica y mieloide en población mayor de 18 años</p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=308544&chapterid=9158" class="btn btn-primary">Ver Presentación </a> </p></div></div></div><div class="disclaimers"> <span>SC-COL-NP-00213<br/>Patrocinado por: AMGEN Biotecnológica S.A.S.<br/>Material dirigido exclusivamente al profesional de la salud<br/>Para mayor información favor contacte a <a href="mailto:medicoscolombia@amgen.com">medicoscolombia@amgen.com</a></span> </div></div>');
+
+$('#material-mod-4').html('<div class="wow fadeInUp"> <div class="disclaimers"> <span>SC-COL-NP-00214<br/>Patrocinado por: AMGEN Biotecnológica S.A.S.<br/>Material dirigido exclusivamente al profesional de la salud<br/>Para mayor información favor contacte a <a href="mailto:medicoscolombia@amgen.com">medicoscolombia@amgen.com</a></span> </div></div>');
+
+$('#material-mod-5').html('<div class="wow fadeInUp"> <div class="disclaimers"> <span>SC-COL-NP-00215<br/>Patrocinado por: AMGEN Biotecnológica S.A.S.<br/>Material dirigido exclusivamente al profesional de la salud<br/>Para mayor información favor contacte a <a href="mailto:medicoscolombia@amgen.com">medicoscolombia@amgen.com</a></span> </div></div>');
